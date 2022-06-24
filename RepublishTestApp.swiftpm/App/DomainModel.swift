00bfc9c01e89c517de0aafb4aa2073bd2884b35e
Adam Zethraeus <adam.zethraeus@includedhealth.com>
2022-06-24 09:48:35 -0700

    Update DomainModel.swift

--- RepublishTestApp.swiftpm/App/DomainModel.swift
+++ RepublishTestApp.swiftpm/App/DomainModel.swift
@@ -1,33 +1,33 @@
 import SwiftUI
 
 final class DomainModel: ObservableObject {
-    
+
     @Published private(set) var count: Int = 0
-    
+
     var isEven: Bool {
         count % 2 == 0
     }
-    
+
     var isZero: Bool {
         count == 0
     }
-    
+
     var isNegative: Bool {
         count < 0
     }
-    
+
     var isPositive: Bool {
         count > 0
     }
-    
+
     var isMax: Bool {
         count == Int.max
     }
-    
+
     var isMin: Bool {
         count == Int.min
     }
-    
+
     var isPrime: Bool {
         switch true {
         case count < 2: return false
@@ -41,9 +41,8 @@ final class DomainModel: ObservableObject {
                 .first == nil
         }
     }
-    
+
     func set(count: Int) {
         self.count = count
     }
 }
-
